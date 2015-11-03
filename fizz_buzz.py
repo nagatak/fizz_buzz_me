7eaf37ed270a8c7eeed96d49db57058ec666c0b3
nagatak <nagatak@hawaii.edu>
2015-11-02 18:55:18 -1000

    fixed fizzbuzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -3,13 +3,19 @@
 def fizz_buzz(number):
     """Return a string representing the number, or 'fizz'/'buzz'/'fizzbuzz'"""
     # TODO - remove 'pass' and write teh codes
-    pass
-
+    if number % 3 == 0 and number % 5 == 0:
+        return 'fizzbuzz'
+    elif number % 3 == 0:
+        return 'fizz'
+    elif number % 5 == 0:
+        return 'buzz'
+    else:
+        return str(number)
 def main():
     """Print sample output of fizz_buzz function"""
     NUMBER = 20
     print("Sample fizz_buzz output for 1 to {0}:".format(NUMBER))
-    for i in range(1, NUMBER):
+    for i in range(1, NUMBER + 1):
         print(fizz_buzz(i))
 
 
